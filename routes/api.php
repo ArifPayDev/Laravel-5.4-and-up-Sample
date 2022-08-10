c53a35ab7c62bb986af1ea4ca409f17960c4e7e1
ba5liel <baslielselamu2018@gmail.com>
2022-08-10 17:04:26 +0300

    Sample added for package V2

--- routes/api.php
+++ routes/api.php
@@ -14,38 +14,56 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/test", function (Request $request) {
-    $arifpay = new ArifPay('your-api');
-    $dt = Carbon::now()->addHours(6);
-    $expired = ArifpaySupport::getExpireDateFromDate($dt);
-    $data = new ArifpayCheckoutRequest(
-        "https://api.arifpay.com",
-        floor(rand() * 10000) . "",
-        'https://api.arifpay.com',
-        'https://gateway.arifpay.net/test/callback',
-        'https://127.0.0.1/beu-laravel',
-        ["CARD"],
-        $expired,
-        [
-            ArifpayCheckoutItem::fromJson([
-                "name" => 'Bannana',
-                "price" => 45,
-                "quantity" => 1,
-            ]),
-        ],
-        [
-            ArifpayBeneficary::fromJson([
-                "accountNumber" => '01320811436100',
-                "bank" => 'AWINETAA',
-                "amount" => 10.0,
-            ]),
-        ],
-    );
-    $session = $arifpay->create($data, new ArifpayOptions(false));
+$arifpay = new ArifPay('your-api-here');
+$dt = Carbon::now()->addHours(6);
+$expired = "2023-01-13T17:09:42.411";
+$data = new ArifpayCheckoutRequest(
+    "https://api.arifpay.com",
+    floor(rand() * 10000) . "",
+    'https://api.arifpay.com',
+    'https://gateway.arifpay.net/test/callback',
+    'https://127.0.0.1/beu-laravel',
+    ["CARD"],
+    $expired,
+    [
+        ArifpayCheckoutItem::fromJson([
+            "name" => 'Bannana',
+            "price" => 10,
+            "quantity" => 1,
+        ]),
+    ],
+    [
+        ArifpayBeneficary::fromJson([
+            "accountNumber" => '01320811436100',
+            "bank" => 'AWINETAA',
+            "amount" => 10.0,
+        ]),
+    ],
+);
+
+Route::get("/test", function (Request $request) use($arifpay, $data) {
+    
+    $session = $arifpay->checkout->create($data, new ArifpayOptions(true));
     return $session;
 });
 
-Route::get("/fetch/{id}", function (Request $request, $id) {
-    $arifpay = new ArifPay('16WD8h8ab2xQjn1IFR6Lk6qFH4K5nc14');
+Route::get("/direct/awash", function (Request $request) use($arifpay, $data) {
+    
+    $session = $arifpay->checkout->create($data, new ArifpayOptions(true));
+
+    return $arifpay->directPay->awash->pay($session->session_id, "251961186323");
+    
+});
+
+Route::get("/direct/telebirr", function (Request $request) use($arifpay, $data) {
+   
+    $session = $arifpay->checkout->create($data, new ArifpayOptions(true));
+
+    return $arifpay->directPay->telebirr->pay($session->session_id);
+    
+});
+
+Route::get("/fetch/{id}", function (Request $request, $id) use($arifpay, $data) {
+    $arifpay = new ArifPay('your-api-here');
     return $arifpay->fetch($id,new ArifpayOptions(true));
 });
