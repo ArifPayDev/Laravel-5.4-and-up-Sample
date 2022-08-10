7a4969df371288644ba96501c3f0580c5a3dc94e
ba5liel <baslielselamu2018@gmail.com>
2022-08-10 15:00:51 +0300

    api route

--- routes/api.php
+++ routes/api.php
@@ -8,21 +8,9 @@ use Arifpay\Arifpay\Lib\ArifpayOptions;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 
 Route::get("/test", function (Request $request) {
     $arifpay = new ArifPay('16WD8h8ab2xQjn1IFR6Lk6qFH4K5nc14');
