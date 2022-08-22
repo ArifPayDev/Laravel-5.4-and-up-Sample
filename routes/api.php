e0d7e976c343bd41c98c6a74115320a41575938a
ba5liel <baslielselamu2018@gmail.com>
2022-08-22 11:56:23 +0300

    Cancle Session  Example

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-$arifpay = new ArifPay('your-api-here');
+$arifpay = new ArifPay('Your-api-key');
 $dt = Carbon::now()->addHours(6);
 $expired = "2023-01-13T17:09:42.411";
 $data = new ArifpayCheckoutRequest(
@@ -23,7 +23,7 @@ $data = new ArifpayCheckoutRequest(
     'https://api.arifpay.com',
     'https://gateway.arifpay.net/test/callback',
     'https://127.0.0.1/beu-laravel',
-    ["CARD"],
+    ["TELEBIRR"],
     $expired,
     [
         ArifpayCheckoutItem::fromJson([
@@ -43,27 +43,39 @@ $data = new ArifpayCheckoutRequest(
 
 Route::get("/test", function (Request $request) use($arifpay, $data) {
     
-    $session = $arifpay->checkout->create($data, new ArifpayOptions(true));
+    $session = $arifpay->checkout->create($data, new ArifpayOptions(false));
     return $session;
 });
 
 Route::get("/direct/awash", function (Request $request) use($arifpay, $data) {
     
-    $session = $arifpay->checkout->create($data, new ArifpayOptions(true));
+    $session = $arifpay->checkout->create($data, new ArifpayOptions(false));
 
     return $arifpay->directPay->awash->pay($session->session_id, "251961186323");
+    
+});
+Route::get("/direct/awash/verify/{id}", function (Request $request) use($arifpay, $data) {
+    
+    $session = $arifpay->checkout->create($data, new ArifpayOptions(false));
+    dd($request->get("id"));
+    return $arifpay->directPay->awash->verify($session->session_id, $request->get("id"));
     
 });
 
 Route::get("/direct/telebirr", function (Request $request) use($arifpay, $data) {
    
-    $session = $arifpay->checkout->create($data, new ArifpayOptions(true));
+    $session = $arifpay->checkout->create($data, new ArifpayOptions(false));
 
     return $arifpay->directPay->telebirr->pay($session->session_id);
     
 });
 
 Route::get("/fetch/{id}", function (Request $request, $id) use($arifpay, $data) {
-    $arifpay = new ArifPay('your-api-here');
-    return $arifpay->fetch($id,new ArifpayOptions(true));
+    $arifpay = new ArifPay('Your-api-key');
+    return $arifpay->checkout->fetch($id,new ArifpayOptions(false));
+});
+
+Route::get("/cancel/{id}", function (Request $request, $id) use($arifpay, $data) {
+    $arifpay = new ArifPay('Your-api-key');
+    return $arifpay->checkout->cancel($id,new ArifpayOptions(false));
 });
